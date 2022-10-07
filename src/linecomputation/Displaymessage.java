1dfdda97bae7910dae7cdc99a6e1f8055ff9c326
susmitha <susmithareddychinthala024@gmail.com>
2022-10-07 19:41:38 +0530

    Added the display message of Welcome to Line

---
+++ src/linecomputation/Displaymessage.java
@@ -0,0 +1,7 @@
+package linecomputation;
+
+public class Displaymessage {
+    public static void main(String[] args ) {
+        System.out.println("Welcome to Line\n" + "Comparison Computation ");
+    }
+}
